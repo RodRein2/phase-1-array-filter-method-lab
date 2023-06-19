f3f6fde63d461839234194b7f29c273e9183bbdd
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-06-19 11:40:29 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+const driver = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(driversList, searchString){
+    const lowercaseSearch = searchString.toLowerCase();
+    return driversList.filter(driver => driver.toLowerCase().includes(lowercaseSearch))
+}
+function fuzzyMatch(driversList, searchString){
+    return driversList.filter(driver => 
+        driver.toLowerCase().startsWith(searchString.toLowerCase())
+    );
+}
+const matchingDrivers = fuzzyMatch(driver, "Sa");
+
+function matchName(driversList, searchString){
+    return driversList.filter(driver =>
+        driver.name.toLowerCase() === searchString.toLowerCase());
+}
+const driverHometowns = matchName(driver, "Bobby")
+
